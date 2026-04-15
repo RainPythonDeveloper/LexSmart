9429586ea7927725e3fc3e18a7686457d3767fbb
Weegoos <batyr.ashim05@mail.ru>
2026-04-15 18:08:27 +0200

    deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // <-- Добавь эту строку
-  /* остальные опции, если есть */
+  /* config options here */
 };
 
 export default nextConfig;
